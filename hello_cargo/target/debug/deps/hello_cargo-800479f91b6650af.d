c309f76bc614e9917210a1842797aad04fae335a
DanielKool <daniel@familiekool.nl>
2024-05-27 10:40:20 +0200

    cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-800479f91b6650af.d
@@ -0,0 +1,5 @@
+C:\Users\danie\OneDrive\Bureaublad\code\rust\hello_cargo\target\debug\deps\libhello_cargo-800479f91b6650af.rmeta: src\main.rs
+
+C:\Users\danie\OneDrive\Bureaublad\code\rust\hello_cargo\target\debug\deps\hello_cargo-800479f91b6650af.d: src\main.rs
+
+src\main.rs:
